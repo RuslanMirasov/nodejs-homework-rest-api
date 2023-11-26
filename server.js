c8cd192c202ee230284bc49b98a5f4eecdee7a3c
RuslanMirasov <info@mirasov.dev>
2023-11-26 14:40:49 +0100

    deploy problem fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require('./app');
 
 const mongoose = require('mongoose');
 
-// const DB_HOST = 'mongodb+srv://mirasovdev:HMshm0YPQPhvnQ1W@db-contacts.atane6n.mongodb.net/db-contacts?retryWrites=true&w=majority';
 const { DB_HOST } = process.env;
 
 mongoose.set('strictQuery', true);
@@ -10,7 +9,9 @@ mongoose.set('strictQuery', true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(3000, () => {
+      console.log('Database connection successful');
+    });
   })
   .catch(error => {
     console.log(error.messege);
