98b1946c64a593d61e3cbec0a4bab3524e6ec91a
RuslanMirasov <info@mirasov.dev>
2023-11-26 13:43:18 +0100

    start

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose');
+
+// const DB_HOST = 'mongodb+srv://mirasovdev:HMshm0YPQPhvnQ1W@db-contacts.atane6n.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const { DB_HOST } = process.env;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error.messege);
+    process.exit(1);
+  });
